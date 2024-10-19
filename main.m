d88124f65826fcc1139a5e7ec3a869e6a24adb97
Syno <39122959+v-Syno@users.noreply.github.com>
2024-10-20 02:08:30 +1100

    Changes to tomatoes and potatoes. Added in Cat
    Changes to the setting of tomatoes and potatoes. adjusted size and location for testing. added in cat model for collision detection

--- main.m
+++ main.m
@@ -9,7 +9,7 @@ close all;
 set(0,'DefaultFigureWindowStyle','docked')
 view(3)
 
-Fruit = ObjectClass();
+Objects = ObjectClass();
 RobotControl = RobotClass();
 
 %% Setup environment
@@ -29,9 +29,9 @@ surf([-1.8,-1.8;1.8,1.8],[0.8,1.5;0.8,1.5],[0.01,0.01;0.01,0.02] ,'CData',soil ,
 surf([-1.8, 1.8; -1.8, 1.8], [1.8, 1.8; 1.8, 1.8], [1.8, 1.8; 0, 0] ,'CData',wall ,'FaceColor','texturemap'); % Back wall (y = 1.8)
 
 % Trees
-tree = 'treetiny.ply';
-ObjectClass.PlaceObjects2(tree, [-0.5,1,0.01], 'Scale', [3.5,3.5,1.5]); % left tree (big)
-ObjectClass.PlaceObjects2(tree, [0.5,0.8,0.01], 'Scale', [2, 2, 0.6]); % right tree (small)
+tree = 'treeSkinnier.ply';
+ObjectClass.PlaceObjects2(tree, [-0.5,1.1,0.01], 'Scale', [0.2,0.2,0.15]); % left tree (big)
+ObjectClass.PlaceObjects2(tree, [0.5,1,0.01], 'Scale', [0.1, 0.1,0.1]); % right tree (small)
 
 % Boxes
 crate = 'crate.ply';
@@ -40,10 +40,6 @@ vegCrate = crate;
 ObjectClass.PlaceObjects2(crate, [0,0.05,0.05], 'Scale', [0.5,1,0.5], 'Rotate', [0, 0, pi/2]); % unsorted
 ObjectClass.PlaceObjects2(fruitCrate, [-0.5,-0.5,0.05], 'Scale', [0.5,1,0.5]); % fruit
 ObjectClass.PlaceObjects2(vegCrate, [0.5,-0.5,0.05], 'Scale', [0.5,1,0.5]);% veg
-
-% add image of fruit/veg on each sorted crate
-
-% Cows
 
 %% Safety Features
 
@@ -66,112 +62,88 @@ surf([-0.75, 0.25; -0.75, 0.25], [-1.26, -1.26; -1.26, -1.26], [0.5, 0.5; 0.1, 0
 tomato = 'tomato.ply';
 potato = 'potato.ply';
 
-% oranges (place holder for tomatoes) on tree positions
+tomatoSize = [0.07,0.07,0.06];
+potatoSize = [0.06,0.11,0.05];
+
+% tomatoes on trees
 tomatoTreePos = [
-    -0.5, 0.75, 0.3;
-    -0.4,0.7 0.25;
-    0.5,0.7, 0.09;    
+    -0.5, 0.9, 0.15;
+    -0.35,1.1 0.2;
+    0.5,0.95, 0.09;    
     ];
 
-% Use a loop to place all oranges
-for i = 1:size(tomatoTreePos, 1)
-    [orangeObject, orangeVertices] = Fruit.PlaceObjects2(tomato, tomatoTreePos(i, :), 'Scale', [1.5, 1.5, 1.5], 'Rotate', [0, 0, 0]);
-end
+[tomatoObject, tomatoVertices] = Objects.PlaceObjects2(tomato, tomatoTreePos, 'Scale', [1.5, 1.5, 1.5]);
 
-% apples(place holder for potatoes) on tree positions
+% potatoes
 potatoTreePos = [
-    0, 1, 0.03;
-    -0.2, 0.9, 0.03;
-    0.2, 1.3, 0.03;    
+    0, 1, 0.01;
+    -0.2, 0.9, 0.01;
+    0.2, 1.3, 0.01;    
     ];
 
-% Use a loop to place all apples
-for i = 1:size(potatoTreePos, 1)
-    ObjectClass.PlaceObjects2(potato, potatoTreePos(i, :), 'Scale', [1.5, 1.5, 1.5], 'Rotate', [0, 0, 0]);
-end
+[potatoObject, potatoVertices] = Objects.PlaceObjects2(potato, potatoTreePos, 'Scale', [1.5, 1.5, 1.5]);
+
 
 % unsorted box 
 unsortedPos = [
     -0.15, 0.12, 0.015;
     0, 0.12, 0.015;
     0.15, 0.12, 0.015;
-    -0.15, -0.02, 0.015;
-    0, -0.02, 0.015;
-    0.15, -0.02, 0.015;
+    % -0.15, -0.02, 0.015; %uncomment later, just testing
+    % 0, -0.02, 0.015;
+    % 0.15, -0.02, 0.015;
     ];
 
-% oranges sorting box 
-% x: -0.55 to -0.3
-% y: -0.2 to -0.9
-orangeSorted = [
+% tomatoes sorting box 
+tomatoSorted = [
    -0.5, -0.35, 0.015;
    -0.5, -0.5, 0.015;
    -0.5, -0.65, 0.015;
    ];
 
-% apples sorting box
-appleSorted = [
+% potatoes sorting box
+potatoSorted = [
    0.5, -0.35, 0.015;
    0.5, -0.5, 0.015;
    0.5, -0.65, 0.015;
    ];
 
-%% Testing
-
-tomato = 'tomato.ply';
-potato = 'potato.ply';
-
-tomatoPosTest = [
-%     -0.4, 1, 0.01
-%     -0.6, 0.8, 0.01
-%     ];
-% [
-    -0.15, 0.12, 0.015;
-    0, 0.12, 0.015;
-    0.15, 0.12, 0.015;
-    -0.15, -0.02, 0.015;
-    0, -0.02, 0.015;
-    0.15, -0.02, 0.015;
-    ];
-
-% potatoPosTest = [
-%     -0.3, 0.3, 0.01;
-%     0.2, 0.5, 0.01;
-%     ];
-
-for i = 1:size(tomatoPosTest, 1)
-    [orangeObject, orangeVertices] = Fruit.PlaceObjects2(tomato, tomatoPosTest(i, :), 'Scale', [1.5, 1.5, 1.5], 'Rotate', [0, 0, 0]);
-end
-
-for i = 1:size(potatoPosTest, 1)
-    [orangeObject, orangeVertices] = Fruit.PlaceObjects2(potato, potatoPosTest(i, :), 'Scale', [1.5, 1.5, 1.5], 'Rotate', [0, 0, 0]);
-end
+%% Setup obstacles
+cat = 'cat.ply';
+catPos = [0,0,0.01];
+[catObject, catVertices] = Objects.PlaceObjects2(cat, catPos, 'Scale', [1,1,1]);
 
 %% Fruit Sorting with Collision Detection for Panda and UR3
 
 steps = 50;
 
 % Initialize robots and their respective collision functions
-pandaRobot = Panda(transl(0,-0.5,0.01) * trotz(pi/2));
-ur3Robot = LinearUR3e(transl(0.3,0.5,0.01) );
+%sortingBot = Panda(transl(0,-0.5,0.01) * trotz(pi/2));
+harvesterBot = LinearUR3e(transl(0.3,0.5,0.01) );
 
-%% Tree harvesting - Orange
+%% Harvesting Tomatos
 
-for i = 1:size(tomatoPosTest, 1)
-    % Step 1: Move to the position of the orange on the tree
-    poseOrange = tomatoPosTest(i, :);
-    RobotClass.MoveObject(pandaRobot, poseOrange, steps, orangeObject{i}, orangeVertices{i}, false,1);
+for i = 1:size(tomatoTreePos, 1)
+    % Step 1: Move to the position of tomato on the tree. Give room for
+    % gripper
+    tomatoPose = tomatoTreePos(i, :);
+    RobotClass.MoveObject2(harvesterBot, tomatoPose, steps, tomatoObject{i}, tomatoVertices{i}, false);
 
-    % Step 2: Lower the end effector to the orange
+    % Step 2: Close the gripper to grip the tomato
+    tomatoPose = tomatoTreePos(i, :);
+    RobotClass.MoveObject2(harvesterBot, tomatoPose, steps, tomatoObject{i}, tomatoVertices{i}, true);
 
-    % Step 3: Close the gripper to grip the orange
+    % Step 2: move tomato to the unsorted crate pose
+    unsortedCratePose = unsortedPos(i, :) + [0,0,0.1];
+    RobotClass.MoveObject2(harvesterBot, unsortedCratePose, steps, tomatoObject{i}, tomatoVertices{i}, true);
 
-    % Step 4: Lift the orange back up slightly
+    % Step 4: move EE pose to point down (z axis down) and lower tomato in
+    % crate
+    unsortedCratePose = unsortedPos(i, :);
+    RobotClass.MoveObject2(harvesterBot, unsortedCratePose, steps, tomatoObject{i}, tomatoVertices{i}, true);
 
-    % Step 5: Move the orange to the unsorted box position
-
-    % Step 6: Lower the orange into the unsorted box
-
-    % Step 8: Move back to a neutral position (above the unsorted box)
+    % Step 5: release gripper
+    unsortedCratePose = unsortedPos(i, :);
+    RobotClass.MoveObject2(harvesterBot, unsortedCratePose, steps, tomatoObject{i}, tomatoVertices{i}, false);    
 
 end
